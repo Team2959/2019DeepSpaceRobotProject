ce77d1fa7d49e83e6be49399079a8c52953f1083
cwtech2959 <cwtech2959@gmail.com>
2019-01-19 15:04:05 -0500

    Add camera server auto captue

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -9,6 +9,7 @@
 
 #include <frc/commands/Scheduler.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <cameraserver/CameraServer.h>
 
 DriveTrainSubsystem Robot::m_driveTrainSubsystem;
 ExampleSubsystem Robot::m_subsystem;
@@ -18,6 +19,7 @@ void Robot::RobotInit() {
   m_chooser.SetDefaultOption("Default Auto", &m_defaultAuto);
   m_chooser.AddOption("My Auto", &m_myAuto);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  frc::CameraServer::GetInstance()->StartAutomaticCapture();
 }
 
 /**
