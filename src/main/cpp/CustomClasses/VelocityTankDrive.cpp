4598de070116522b0ca56d9a208d6a3edea9bd43
Joel Copi <j.copi@outlook.com>
2019-02-06 09:53:38 -0500

    Formatted and Commenting

--- src/main/cpp/CustomClasses/VelocityTankDrive.cpp
+++ src/main/cpp/CustomClasses/VelocityTankDrive.cpp
@@ -8,13 +8,21 @@
 #include "CustomClasses/VelocityTankDrive.h"
 
 
-VelocityTankDrive::VelocityTankDrive() {
+VelocityTankDrive::VelocityTankDrive()
+{
+    // Set up the follower motor controllers
     m_right2Follower.Follow(m_right1Primary);
     m_left2Follower.Follow(m_left1Primary);
-
+    
+    // Assign a PID controller for each motor
     m_pidControllerLeft = m_left1Primary.GetPIDController();
     m_pidControllerRight = m_right1Primary.GetPIDController();
 
+    // Get the encoder values from each primary controller
+    m_encoderLeft = m_left1Primary.GetEncoder();
+    m_encoderRight = m_right1Primary.GetEncoder();
+
+    // Set the PIDF gains for the left side primary motor controller
     m_pidControllerLeft.SetP(m_proportional);
     m_pidControllerLeft.SetI(m_integral);
     m_pidControllerLeft.SetD(m_derivative);
@@ -22,32 +30,40 @@ VelocityTankDrive::VelocityTankDrive() {
     m_pidControllerLeft.SetFF(m_feedForward);
     m_pidControllerLeft.SetOutputRange(-1, 1);
 
+    // Set the PIDF gains for the right side primary motor controller
     m_pidControllerRight.SetP(m_proportional);
     m_pidControllerRight.SetI(m_integral);
     m_pidControllerRight.SetD(m_derivative);
     m_pidControllerRight.SetIZone(m_iZone);
     m_pidControllerRight.SetFF(m_feedForward);
     m_pidControllerRight.SetOutputRange(-1, 1);
-
-    m_encoderLeft = m_left1Primary.GetEncoder();
-    m_encoderRight = m_right1Primary.GetEncoder();
-
 }
 
 void VelocityTankDrive::SetPointGenerator (double dsp, rev::CANPIDController& pidController, rev::CANEncoder& encoder)
 {
-    double tSlice = 0.02;
-    double current = encoder.GetVelocity();
-    double velocity = dsp;
-    double accel = m_maxAcceleration * tSlice + current;
-    double jerk = m_maxJerk * tSlice * tSlice + current;
-    double min = velocity;
-    if (accel < min)min = accel;
-    if (jerk < min)min = jerk;
+    double tSlice  = 0.02;                  // Assumed rate at which this function is called in seconds
+    double current = encoder.GetVelocity(); // Current velocity
+
+    // Calculating the maximum speed that can be achieved by driving at each limit for 'tslice'
+    // This approach applies each limit independently, rather than using all together
+    double velocity = dsp;                                   // RPM
+    double accel    = m_maxAcceleration * tSlice + current;  // RPM/s
+    double jerk     = m_maxJerk * tSlice * tSlice + current; // RPM/s^2
+
+    // Calculate the minimum of the 3 above variables
+    double min = velocity; // Initially, assume velocity is the minimum
+    if (accel < min)       // Check if acceleration is less than the minimum
+        min = accel;       // If so, set it as the new minimum
+    if (jerk < min)        // Check if jerk is less than the minimum
+        min = jerk;        // If so, set it as the new minimum
+
+    // The minimum of our limits is our set-point to drive at
     pidController.SetReference(min, rev::ControlType::kVelocity);
-  
 }
-void VelocityTankDrive::TankDrive(double left, double right){
-    SetPointGenerator(left, m_pidControllerLeft,m_encoderLeft);
-    SetPointGenerator(right, m_pidControllerRight,m_encoderRight);
+
+void VelocityTankDrive::TankDrive(double left, double right)
+{
+    // Set the lieft and right side sppeds through the SetPointGenerator function
+    SetPointGenerator(left, m_pidControllerLeft, m_encoderLeft);
+    SetPointGenerator(right, m_pidControllerRight, m_encoderRight);
 }
--- src/main/include/CustomClasses/VelocityTankDrive.h
+++ src/main/include/CustomClasses/VelocityTankDrive.h
@@ -13,45 +13,47 @@
 
 class VelocityTankDrive: public frc::RobotDriveBase
 {
-    private:
+private:
+    // Motor controllers used in the robot drive
     rev::CANSparkMax m_right1Primary { kRight1CanSparkMaxMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
     rev::CANSparkMax m_right2Follower { kRight2CanSparkMaxMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
     rev::CANSparkMax m_left1Primary { kLeft1CanSparkMaxMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
     rev::CANSparkMax m_left2Follower { kLeft2CanSparkMaxMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
     
+    // PID Controllers
     rev::CANPIDController m_pidControllerLeft;
     rev::CANPIDController m_pidControllerRight;
 
-    double m_maxRPM = 5676*0.85;
-
-    double m_gearRatio = 1 / 6.11;
-
-    double m_maxTorque = 40.623744*0.85;
-
-    double m_maxAcceleration = 2207.07*0.85; //RPM/s
-
-    double m_maxVelocity = m_maxRPM;
-
-    double m_maxJerk = 50000;
-
-    double m_proportional = 0.0005;
-
-    double m_integral = 0.000001;
-
-    double m_derivative = 0;
-
-    double m_feedForward = 0;
-
-    double m_iZone = 0;
-
-
-    void SetPointGenerator(double dsp, rev::CANPIDController& pidController, rev::CANEncoder& encoder);
-
+    // Encoders
     rev::CANEncoder m_encoderLeft;
     rev::CANEncoder m_encoderRight;
 
-    public:
+    // Characterizations of our robot drive train in controlled units (RPM & seconds)
+    double m_maxRPM          = 5676.0 * 0.85;    // RPM
+    double m_gearRatio       = 1.0 / 6.11;       // Unused
+    double m_maxTorque       = 40.623744 * 0.85  // N*m
+    double m_maxAcceleration = 2207.07 * 0.85;   // RPM/s
+    double m_maxVelocity     = m_maxRPM;         // RPM
+    double m_maxJerk         = 50000.0;          // RPM/s^2
+
+    // Control Loop (PIDF) gains
+    double m_proportional = 0.0005;
+    double m_integral     = 0.000001;
+    double m_derivative   = 0.0;
+    double m_feedForward  = 0.0;
+    double m_iZone        = 0.0;
+
+    // SetPointGenerator is the singular interface for each motor controller.  
+    /* It shall accepts a desired set point and determines a responsible next set point 
+       for the motor control loop, accounting for current speed and all applicable limits. */
+    void SetPointGenerator (double dsp, rev::CANPIDController& pidController, rev::CANEncoder& encoder);
+
+public:
+    // Constructor
     VelocityTankDrive();
-    void TankDrive(double left, double right);
-    
+
+    // TankDrive is an interface similar to that of the FRC differential drive class. 
+    /* It shall accept a speed for each side of the robot drive in real world values (RPM),
+       as opposed to percentages. */
+    void TankDrive (double left, double right);
 };
