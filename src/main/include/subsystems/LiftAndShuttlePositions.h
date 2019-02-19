aa39203f3bb335bf799663dbd03a8fa0b7f4ca28
cwtech2959 <cwtech2959@gmail.com>
2019-02-19 00:00:54 -0500

    Switch to low gear

--- src/main/include/subsystems/LiftAndShuttlePositions.h
+++ src/main/include/subsystems/LiftAndShuttlePositions.h
@@ -6,8 +6,8 @@ constexpr int kShuttleFrontPosition = 5000;
 constexpr int kShuttleRearPosition = -3000;
 
 constexpr int kLiftFloorPosition = 0;
-constexpr int kLiftBottomPosition = 2;
-constexpr int kLiftCargoShipPosition = 12;
-constexpr int kLiftMiddlePosition = 20;
-constexpr int kLiftTopPosition = 27.5;
-constexpr int kLiftRemoveHatchFromWallPosition = kLiftBottomPosition + 3;
+constexpr int kLiftBottomPosition = 4;
+constexpr int kLiftCargoShipPosition = 24;
+constexpr int kLiftMiddlePosition = 40;
+constexpr int kLiftTopPosition = 55;
+constexpr int kLiftRemoveHatchFromWallPosition = kLiftBottomPosition + 6;
