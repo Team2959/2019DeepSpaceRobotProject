80de5c03fd72d91a359af87f953343fa63fb1a6f
cwtech2959 <cwtech2959@gmail.com>
2019-03-25 18:19:44 -0400

    Pid Paramters for the climb arms.

--- src/main/cpp/subsystems/ClimbSubsystem.cpp
+++ src/main/cpp/subsystems/ClimbSubsystem.cpp
@@ -16,10 +16,10 @@ constexpr double kAcceleration = 4500;
 ClimbSubsystem::ClimbSubsystem() : Subsystem("ClimbSubsystem")
 {
     m_right.GetSlotConfigs(m_pidConfig);
-    m_pidConfig.kP = 0.1;
+    m_pidConfig.kP = 0.001;
     m_pidConfig.kI = 0.0;
     m_pidConfig.kD = 0;
-    m_pidConfig.kF = 0.057;
+    m_pidConfig.kF = 0;
     // m_pidConfig.integralZone = x;
     // m_pidConfig.closedLoopPeakOutput = 1.0;
     // m_pidConfig.allowableClosedloopError = 128;
